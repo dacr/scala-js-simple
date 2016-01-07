0f931ef396f319a0480794dda9e82dba319419ca
crosson.david@gmail.com <crosson.david@gmail.com>
2016-01-07 22:52:12 +0100

    playing with d3

--- build.sbt
+++ build.sbt
@@ -4,7 +4,10 @@ name := "scala-js-simple"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
+libraryDependencies ++= Seq(
+  "be.doeraene" %%% "scalajs-jquery" % "0.8.0",
+  "org.singlespaced" %%% "scalajs-d3" % "0.1.2"
+)
 
 skip in packageJSDependencies := false
 
--- src/main/scala/simple/SimpleApp.scala
+++ src/main/scala/simple/SimpleApp.scala
@@ -8,8 +8,55 @@ import org.scalajs.jquery.jQuery
 import org.scalajs.jquery.JQueryEventObject
 import scala.scalajs.js.Dynamic.{ global => g }
 
+import scala.scalajs.js
+import org.singlespaced.d3js._
+import org.singlespaced.d3js.Ops._
+
 object SimpleApp extends JSApp {
 
+  def test(): Unit = {
+    /**
+     * Adapted from http://thecodingtutorials.blogspot.ch/2012/07/introduction-to-d3.html
+     */
+    val graphHeight = 200
+
+    //The width of each bar.
+    val barWidth = 40
+
+    //The distance between each bar.
+    val barSeparation = 10
+
+    //The maximum value of the data.
+    val maxData = 50
+
+    //The actual horizontal distance from drawing one bar rectangle to drawing the next.
+    val horizontalBarDistance = barWidth + barSeparation
+
+    //The value to multiply each bar's value by to get its height.
+    val barHeightMultiplier = graphHeight / maxData;
+
+    //Color for start
+    val c = d3.rgb("DarkSlateBlue")
+
+    val rectXFun = (d: Int, i: Int) => i * horizontalBarDistance
+    val rectYFun = (d: Int) => graphHeight - d * barHeightMultiplier
+    val rectHeightFun = (d: Int) => d * barHeightMultiplier
+    val rectColorFun = (d: Int, i: Int) => c.brighter(i * 0.5).toString
+
+    val svg = d3.select("#playground").append("svg").attr("width", "100%").attr("height", "200px")
+    val sel = svg.selectAll("rect").data(js.Array(8, 22, 31, 36, 48, 17, 25))
+    sel.enter()
+      .append("rect")
+      .attr("x", rectXFun)
+      .attr("y", rectYFun)
+      .attr("width", barWidth)
+      .attr("height", rectHeightFun)
+      .style("fill", rectColorFun)
+  }
+
+  
+  
+  
   //@JSExport
   def addClickedMessage(): Unit = {
     jQuery("body").append("You clicked the button!<br/>")
@@ -34,10 +81,10 @@ object SimpleApp extends JSApp {
 
   var inc = 1
   //var cx=100
-  def animate(grc:org.scalajs.jquery.JQuery) {
+  def animate(grc: org.scalajs.jquery.JQuery) {
     //println(grc.attr("cx"))
     //val cx = grc.attr("cx").toInt + inc
-    var cx=grc.attr("cx").toInt
+    var cx = grc.attr("cx").toInt
     cx = cx + inc
     if (cx < 60) inc = 2
     if (cx > 400) inc = -2
@@ -55,6 +102,7 @@ object SimpleApp extends JSApp {
 
   def main(): Unit = {
     jQuery(setupUI _)
+    test()
     println("done")
   }
 
